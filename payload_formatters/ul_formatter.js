cc23467d0c91a092c577e33d63de270f0e0ae895
Tom Wyatt <tom@span-eng.com>
2023-03-19 16:31:46 +1000

    Updated payload formatter

--- payload_formatters/ul_formatter.js
+++ payload_formatters/ul_formatter.js
@@ -18,139 +18,88 @@ function decodeUplink(input) {
 
 }
 
-// Taken from here : https://github.com/dragino/dragino-end-node-decoder/blob/main/LSN50v2-S31%26S31B/LSN50v2-S31-Chirpstack%20v4%20decoder.txt
+// Taken from here : https://github.com/dragino/dragino-end-node-decoder/blob/main/LSN50v2-D20-D22-D23/LSN50v2-D20-chirpstack-Decoder.txt
 
 function Decode(fPort, bytes) {
-  var decode={
-      //Work mode 
-      Work_mode:
-      {
-        "0":"IIC",
-        "1":"Distance",
-        "2":"3ADC",
-        "3":"3DS18B20",
-        "4":"Weight",
-        "5":"Count"
-      }[(bytes[6] & 0x7C)>>2],
   
-      //Battery,units:V
-      BatV:
-      {
-        "0": (bytes[0]<<8 | bytes[1])/1000,
-        "1": (bytes[0]<<8 | bytes[1])/1000,
-        "2": bytes[11]/10,
-        "3": (bytes[0]<<8 | bytes[1])/1000,
-        "4": (bytes[0]<<8 | bytes[1])/1000,
-        "5": (bytes[0]<<8 | bytes[1])/1000,
-      }[(bytes[6] & 0x7C)>>2],
-      
-      //DS18B20,PB3,units:�?
-      TempC1:
-      {
-        "0": ((bytes[2]<<24>>16 | bytes[3])/10).toFixed(2),
-        "1": ((bytes[2]<<24>>16 | bytes[3])/10).toFixed(2),
-        "3": ((bytes[2]<<24>>16 | bytes[3])/10).toFixed(2),
-        "4": ((bytes[2]<<24>>16 | bytes[3])/10).toFixed(2),
-        "5": ((bytes[2]<<24>>16 | bytes[3])/10).toFixed(2),
-      }[(bytes[6] & 0x7C)>>2],
-      
-      //ADC Channel 0,PA0,units:V
-      ADC_CH0V:
-      {
-        "0":(bytes[4]<<8 | bytes[5])/1000,
-        "1":(bytes[4]<<8 | bytes[5])/1000,
-        "2": (bytes[0]<<8 | bytes[1])/1000,
-        "3":(bytes[4]<<8 | bytes[5])/1000,
-        "4":(bytes[4]<<8 | bytes[5])/1000,
-        "5":(bytes[4]<<8 | bytes[5])/1000,      
-      }[(bytes[6] & 0x7C)>>2],
-      
-      //Digital Input Status,PA12
-      Digital_IStatus:
-      {
-        "0":(bytes[6] & 0x02)? "H":"L",
-        "1":(bytes[6] & 0x02)? "H":"L",
-        "2":(bytes[6] & 0x02)? "H":"L",
-        "3":(bytes[6] & 0x02)? "H":"L",
-        "4":(bytes[6] & 0x02)? "H":"L",
-        "5":(bytes[6] & 0x02)? "H":"L",
-      }[(bytes[6] & 0x7C)>>2],    
-      
-      //GPIO_MODE_IT_FALLING,PB14
-      EXTI_Trigger:
-      {
-        "0":(bytes[6] & 0x01)? "TRUE":"FALSE",
-        "1":(bytes[6] & 0x01)? "TRUE":"FALSE",
-        "2":(bytes[6] & 0x01)? "TRUE":"FALSE",
-        "3":(bytes[6] & 0x01)? "TRUE":"FALSE",
-        "4":(bytes[6] & 0x01)? "TRUE":"FALSE",
-      }[(bytes[6] & 0x7C)>>2],    
-      
-      //Status of door sensor,PB14 
-      Door_status:
-      {
-        "0": (bytes[6] & 0x80)? "CLOSE":"OPEN",
-        "1": (bytes[6] & 0x80)? "CLOSE":"OPEN",
-        "2": (bytes[6] & 0x80)? "CLOSE":"OPEN",
-        "3": (bytes[6] & 0x80)? "CLOSE":"OPEN",
-        "4": (bytes[6] & 0x80)? "CLOSE":"OPEN",
-      }[(bytes[6] & 0x7C)>>2],
-      
-      //SHT2X,SHT3X temperature,PB6,PB7,units:�?
-      TempC_SHT:
-      {
-        "0":((bytes[7]<<24>>16 | bytes[8])/10).toFixed(2),
-        "2":((bytes[7]<<24>>16 | bytes[8])/10).toFixed(2),
-      }[(bytes[6] & 0x7C)>>2],    
-      //SHT2X,SHT3X Humidity,PB6,PB7,units:%
-      Hum_SHT:
-      {
-        "0": ((bytes[9]<<8 | bytes[10])/10) .toFixed(1),
-        "2": ((bytes[9]<<8 | bytes[10])/10) .toFixed(1),
-      }[(bytes[6] & 0x7C)>>2],
-      
-      //Distance,PA11,PB12,units:cm;
-      Distance:
-      {
-        "1":((bytes[7]<<8 | bytes[8])/10) .toFixed(1),
-      }[(bytes[6] & 0x7C)>>2],
-      
-      //ADC Channel 1,PA1,units:V
-      ADC_CH1V:
-      {
-        "2":(bytes[2]<<8 | bytes[3])/1000,
-      }[(bytes[6] & 0x7C)>>2],
-      //ADC Channel 4,PA4,units:V
-      ADC_CH4V:
-      {
-        "2":(bytes[4]<<8 | bytes[5])/1000,
-      }[(bytes[6] & 0x7C)>>2],
-    
-      //DS18B20,PA9,units:�?
-      TempC2:
-      {
-        "3":((bytes[7]<<24>>16 | bytes[8])/10).toFixed(2),
-      }[(bytes[6] & 0x7C)>>2],
-      //DS18B20,PA10,units:�?
-      TempC3:
-      {
-        "3":((bytes[9]<<24>>16 | bytes[10])/10).toFixed(2),
-      }[(bytes[6] & 0x7C)>>2],
-    
-      //Weight,PA11,PB12,units:g;
-      Weight:
-      {
-        "4":(bytes[7]<<24>>16 | bytes[8]),
-      }[(bytes[6] & 0x7C)>>2],
-      
-      //interrupt count
-      Count:
-      {
-        "5":(bytes[7]<<24 | bytes[8]<<16 | bytes[9]<<8 | bytes[10]),
-      }[(bytes[6] & 0x7C)>>2],
+  var mode=(bytes[6] & 0x7C)>>2;
+  var decode = {};
+
+  if((mode!=2)&&(mode!=31)){
+    decode.BatV=(bytes[0]<<8 | bytes[1])/1000;
+    decode.TempC1= parseFloat(((bytes[2]<<24>>16 | bytes[3])/10).toFixed(2));
+    decode.ADC_CH0V=(bytes[4]<<8 | bytes[5])/1000;
+    decode.Digital_IStatus=(bytes[6] & 0x02)? "H":"L";
+    if(mode!=6)
+    {
+    decode.EXTI_Trigger=(bytes[6] & 0x01)? "TRUE":"FALSE";
+    decode.Door_status=(bytes[6] & 0x80)? "CLOSE":"OPEN";
+    }
   }
 
 
+  if(mode=='0'){
+    decode.Work_mode="IIC";
+    if((bytes[9]<<8 | bytes[10])===0){
+      decode.Illum=(bytes[7]<<24>>16 | bytes[8]);
+    }
+    else {
+      decode.TempC_SHT=parseFloat(((bytes[7]<<24>>16 | bytes[8])/10).toFixed(2));
+      decode.Hum_SHT=parseFloat(((bytes[9]<<8 | bytes[10])/10).toFixed(1));
+    }
+  }
+  else if(mode=='1'){
+    decode.Work_mode=" Distance";
+    decode.Distance_cm=parseFloat(((bytes[7]<<8 | bytes[8])/10) .toFixed(1));
+    if((bytes[9]<<8 | bytes[10])!=65535){
+      decode.Distance_signal_strength=parseFloat((bytes[9]<<8 | bytes[10]) .toFixed(0));
+    }
+  }
+  else if(mode=='2'){
+    decode.Work_mode=" 3ADC";
+    decode.BatV=bytes[11]/10;
+    decode.ADC_CH0V=(bytes[0]<<8 | bytes[1])/1000;
+    decode.ADC_CH1V=(bytes[2]<<8 | bytes[3])/1000;
+    decode.ADC_CH4V=(bytes[4]<<8 | bytes[5])/1000;
+    decode.Digital_IStatus=(bytes[6] & 0x02)? "H":"L";
+    decode.EXTI_Trigger=(bytes[6] & 0x01)? "TRUE":"FALSE";
+    decode.Door_status=(bytes[6] & 0x80)? "CLOSE":"OPEN";
+    if((bytes[9]<<8 | bytes[10])===0) {
+      decode.Illum=(bytes[7]<<24>>16 | bytes[8]);
+    }
+    else
+    {
+    decode.TempC_SHT=parseFloat(((bytes[7]<<24>>16 | bytes[8])/10).toFixed(2));
+    decode.Hum_SHT=parseFloat(((bytes[9]<<8 | bytes[10])/10) .toFixed(1));
+    }
+  }
+  else if(mode=='3'){
+    decode.Work_mode="3DS18B20";
+    decode.TempC2=parseFloat(((bytes[7]<<24>>16 | bytes[8])/10).toFixed(2));
+    decode.TempC3=parseFloat(((bytes[9]<<24>>16 | bytes[10])/10) .toFixed(1));
+  }
+  else if(mode=='4'){
+    decode.Work_mode="Weight";
+    decode.Weight=(bytes[7]<<24>>16 | bytes[8]);
+  }
+  else if(mode=='5'){
+    decode.Work_mode="Count";
+    decode.Count=(bytes[7]<<24 | bytes[8]<<16 | bytes[9]<<8 | bytes[10]);
+  }
+  else if(mode=='31'){
+    decode.Work_mode="ALARM";
+    decode.BatV=(bytes[0]<<8 | bytes[1])/1000;
+    decode.TempC1= parseFloat(((bytes[2]<<24>>16 | bytes[3])/10).toFixed(2));
+    decode.TempC1MIN= bytes[4]<<24>>24;
+    decode.TempC1MAX= bytes[5]<<24>>24; 
+    decode.SHTEMPMIN= bytes[7]<<24>>24;
+    decode.SHTEMPMAX= bytes[8]<<24>>24;   
+    decode.SHTHUMMIN= bytes[9];
+    decode.SHTHUMMAX= bytes[10];    
+  }
+
+
+  
   decode.doover_channels = {
     ui_state : {
         state : {
